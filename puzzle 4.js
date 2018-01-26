94da87c6b89e695d4be6a88f005fc809a4d9fded
lkkbrian <35850909+lkkbrian@users.noreply.github.com>
2018-01-26 13:20:45 -0800

    Create puzzle 4.js

---
+++ puzzle 4.js
@@ -0,0 +1,21 @@
+down();
+down();
+down();
+down();
+right();
+Function(Transport) {
+  var(A)=GetColor(green)
+  var(B)=Get Color(yellow)
+  if(A)==green
+    down();
+    SetColor(A)
+    right();
+  if(B)==yellow
+    down();
+    SetColor(B)
+    right();
+}
+right();
+up();
+Function(Transport)
+right();
