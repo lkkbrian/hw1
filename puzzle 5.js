d2f38af5d6de3b98ade0d8b4baca6ca77631d964
lkkbrian <35850909+lkkbrian@users.noreply.github.com>
2018-01-26 13:33:22 -0800

    Create puzzle 5.js

---
+++ puzzle 5.js
@@ -0,0 +1,18 @@
+right();
+right();
+GetColor();
+var(A)=GetColor(pink)
+if(var A==pink){up};
+right();
+right();
+GetColor();
+var(B)=GetColor(blue)
+if(var B==blue){down};
+right();
+right();
+GetColor();
+var(B)=GetColor(blue)
+if(var B==blue){down};
+right();
+right();
+right();
