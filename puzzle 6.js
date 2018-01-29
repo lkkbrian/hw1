d3ab61c5481c1134faf2c03c47d5c31eb773a76f
lkkbrian <35850909+lkkbrian@users.noreply.github.com>
2018-01-29 11:47:12 -0800

    Update puzzle 6.js

--- puzzle 6.js
+++ puzzle 6.js
@@ -1,15 +1,33 @@
+
 right();
-GetColor();
-var(A)=GetColor(pink);
-var(B)=GetColor(blue;
-if(A==pink){up};
-if(B==blue){down};
-up();
-up();
-up();
-GetColor();
-var(A)=GetColor(pink);
-var(B)=GetColor(blue;
-if(A==pink){left};
-if(B==blue){right};
-up();
+getColor();
+var A = getColor();
+function square (){
+  if (getColor() == "red"){
+  	up();
+  	up();
+  	up();
+	} else {
+  	down();
+  	down();
+  	down();
+}
+}
+square();
+getColor();
+ if(getColor() == "red") {
+  left();
+  down();
+}
+ if (getColor() == "blue") {
+  right();
+  up(); 
+} 
+   if(getColor() == "blue") {
+  right();
+  down();
+}
+if(getColor() == "red") {
+ left();
+ up();
+}
